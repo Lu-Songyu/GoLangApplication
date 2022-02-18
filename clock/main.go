61640128ca9ff5b9041963c7d8820414462409d8
songyu lu <songyulu99@gmail.com>
2022-02-17 19:08:50 -0800

    animate clock

--- clock/main.go
+++ clock/main.go
@@ -3,11 +3,21 @@ package main
 import (
 	"fmt"
 	"time"
+
+	"github.com/inancgumus/screen"
 )
 
 func main() {
 	// for keeping things easy to read and type-safe
 	type placeholder [5]string
+
+	colon := placeholder{
+		"   ",
+		" █ ",
+		"   ",
+		" █ ",
+		"   ",
+	}
 
 	zero := placeholder{
 		"███",
@@ -90,22 +100,37 @@ func main() {
 	}
 
 	digits := [...]placeholder{
-		zero, one, two, three, four, five, six, seven, eight, nine,
+		colon, zero, one, two, three, four, five, six, seven, eight, nine,
 	}
+	count := 0
 
-	hours, minutes, _ := time.Now().Clock()
+	screen.Clear()
 
-	clock := [...]placeholder{
-		digits[hours/10],
-		digits[hours%10],
-		digits[minutes/10],
-		digits[minutes%10],
-	}
+	for count >= 0 {
 
-	for line := range clock[0] {
-		for digit := range clock {
-			fmt.Print(clock[digit][line], "  ")
+		screen.MoveTopLeft()
+		hours, minutes, seconds := time.Now().Clock()
+		time.Sleep(time.Second)
+
+		clock := [...]placeholder{
+			digits[hours/10],
+			digits[hours%10],
+			colon,
+			digits[minutes/10],
+			digits[minutes%10],
+			colon,
+			digits[seconds/10],
+			digits[seconds%10],
 		}
-		fmt.Println()
+
+		// for each digit
+		for line := range clock[0] {
+			for digit := range clock {
+				//fmt.Print(digit, line)
+				fmt.Print(clock[digit][line], " ")
+			}
+			fmt.Println(" ")
+		}
+
 	}
 }
